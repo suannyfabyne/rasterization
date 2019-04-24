3685107acb834706e1f7cdae715cf403ef804181
Suanny <suanny_fabyne@hotmail.com>
2019-04-23 21:20:06 -0300

    adding tarefa_2 to repository

--- tarefa_1/main.cpp
+++ tarefa_1/main.cpp
@@ -6,9 +6,10 @@ void MyGlDraw(void)
 	Pixel Vertice_1(128, 406, 255, 0, 0, 255);
 	Pixel Vertice_2(256, 106, 0, 255, 0, 255);
 	Pixel Vertice_3(384, 406, 0, 0, 255, 255);
-
 	DrawTriangle(Vertice_1, Vertice_2, Vertice_3);
+
 	int j = 0;
+
 	for (int i = 128; i < 385; ++i)
 	{
 		Pixel Vertice_1(i, 406, 255-j, 0, 0+j, 255);
@@ -17,7 +18,6 @@ void MyGlDraw(void)
 		DrawLine(Vertice_1, Vertice_2);
 	}
 
-	
 } 
 //-----------------------------------------------------------------------------
 int main(int argc, char **argv)
